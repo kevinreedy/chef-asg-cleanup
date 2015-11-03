1bad6d7a9fc68eaaade93a223fd567aa6028c7ea
Kevin Reedy <kevinreedy@gmail.com>
2015-11-03 17:41:23 -0600

    add more paths since include_files isn't recursive

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,16 @@ module.exports = function(grunt) {
         options: {
           // Task-specific options go here.
           include_files: [
-            'node_modules/chef-api/node_modules/ursa/build/*'
+            'node_modules/chef-api/node_modules/ursa/build/*',
+            'node_modules/chef-api/node_modules/ursa/build/Release/*',
+            'node_modules/chef-api/node_modules/ursa/build/Release/.deps/*',
+            'node_modules/chef-api/node_modules/ursa/build/Release/.deps/Release/*',
+            'node_modules/chef-api/node_modules/ursa/build/Release/.deps/Release/obj.target/*',
+            'node_modules/chef-api/node_modules/ursa/build/Release/.deps/Release/obj.target/ursaNative/*',
+            'node_modules/chef-api/node_modules/ursa/build/Release/.deps/Release/obj.target/ursaNative/src/*',
+            'node_modules/chef-api/node_modules/ursa/build/Release/obj.target/*',
+            'node_modules/chef-api/node_modules/ursa/build/Release/obj.target/ursaNative/*',
+            'node_modules/chef-api/node_modules/ursa/build/Release/obj.target/ursaNative/src/*'
           ]
         }
       }
